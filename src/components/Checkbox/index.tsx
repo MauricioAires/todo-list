58ccd17a9b9441dc32306d263ec86a2af39a287b
MauricioAires <mauricioaires015@gmail.com>
2023-01-04 23:37:31 -0300

    feat: action check and uncheck task

--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -2,8 +2,17 @@ import S from './styles.module.css'
 
 interface CheckBoxProps {
   status?: 'uncheck' | 'check'
+  onCheck: () => void
 }
 
-export function CheckBox({ status = 'uncheck' }: CheckBoxProps) {
-  return <input className={S.checkbox} value={status} type="checkbox" />
+export function CheckBox({ status = 'uncheck', onCheck }: CheckBoxProps) {
+  return (
+    <input
+      className={S.checkbox}
+      value={status}
+      checked={status === 'check'}
+      type="checkbox"
+      onChange={() => onCheck()}
+    />
+  )
 }
