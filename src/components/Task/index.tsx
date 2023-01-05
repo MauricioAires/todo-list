06e196331d644ed9cfe11072123e093e6bbb3c28
MauricioAires <mauricioaires015@gmail.com>
2023-01-04 23:37:08 -0300

    feat: add task component

--- src/components/Task/index.tsx
+++ src/components/Task/index.tsx
@@ -4,18 +4,29 @@ import { DeleteButton } from '../DeleteButton'
 
 import S from './styles.module.css'
 
-export function Task() {
+interface TaskProps {
+  id: number
+  status: 'check' | 'uncheck'
+  content: string
+  onToggleTaskStatus: (id: number) => void
+  onRemoveTask: (id: number) => void
+}
+
+export function Task({
+  id,
+  content,
+  status,
+  onToggleTaskStatus,
+  onRemoveTask
+}: TaskProps) {
   return (
-    <div className={S.task}>
-      <CheckBox />
-      <p>
-        Integer urna interdum massa libero auctor neque turpis turpis semper.
-        Duis vel sed fames integer. Integer urna interdum massa libero auctor
-        neque turpis turpis semper. Duis vel sed fames integer. Integer urna
-        interdum massa libero auctor neque turpis turpis semper. Duis vel sed
-        fames integer.
-      </p>
-      <DeleteButton />
+    <div
+      className={`${S.task} ${status === 'check' && S.taskCompleted}`}
+      data-index={id}
+    >
+      <CheckBox status={status} onCheck={() => onToggleTaskStatus(id)} />
+      <p>{content}</p>
+      <DeleteButton onClick={() => onRemoveTask(id)} />
     </div>
   )
 }
